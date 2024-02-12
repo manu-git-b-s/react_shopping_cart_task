26ca4fc6de87cfae65487364617289b208d8e849
Manupriyan <manupriyan722@gmail.com>
2024-02-13 00:41:00 +0530

    task completed

---
+++ README.md
@@ -0,0 +1,38 @@
+# REACT SHOPPING CART WEB APP
+
+# DAY-23-TASK
+
+This React application is created using vite and features a simple shopping cart app.User interface shows 8 products of different prices and to add the product `add to cart` button can be clicked then the product will be added to cart using useState and was built in a dynamic way with JSX,and Bootstrap.
+
+### Responsive Web Design:
+
+- The app is designed to be responsive, ensuring a consistent and optimized user interface across various devices and screen sizes.
+
+### Bootstrap Integration:
+
+- Incorporates Bootstrap for styling and layout components, contributing to a modern and visually appealing UI.
+
+## Usage
+
+1. Clone the repository to your local machine.
+2. cd into your cloned repository.
+3. Execute npm i to add node modules files.
+4. Execute npm run dev to start the application
+
+## Technologies Used
+
+- Vite
+- React
+- JSX
+- Bootstrap
+
+## How to Contribute
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make changes and submit a pull request.
+4. Your contributions are highly appreciated!
+
+## Live Demo
+
+Check out the live demo: [React Shopping Cart](https://react-price-card-manu.netlify.app/)
---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import NavBar from "./components/Navbar/NavBar";
+import Products from "./components/Products/Products";
+
+function App() {
+  // products list data
+  const products = [
+    {
+      name: "Fancy Product",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$40.00 - $80.00",
+      rating: 0,
+      sale: false,
+      original: false,
+    },
+    {
+      name: "Special Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$18.00",
+      rating: 5,
+      sale: true,
+      original: true,
+      originalPrice: "$20.00",
+    },
+    {
+      name: "Sale Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$25.00",
+      rating: 0,
+      sale: true,
+      original: true,
+      originalPrice: "$50.00",
+    },
+    {
+      name: "Popular Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$40.00",
+      rating: 5,
+      sale: false,
+      original: false,
+      originalPrice: "",
+    },
+    {
+      name: "Sale Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$25.00",
+      rating: 0,
+      sale: true,
+      original: true,
+      originalPrice: "$50.00",
+    },
+    {
+      name: "Fancy Product",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$120.00 - $280.00",
+      rating: 0,
+      sale: false,
+      original: false,
+    },
+    {
+      name: "Special Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$18.00",
+      rating: 5,
+      sale: true,
+      original: true,
+      originalPrice: "$20.00",
+    },
+    {
+      name: "Popular Item",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      price: "$40.00",
+      rating: 5,
+      sale: false,
+      original: false,
+      originalPrice: "",
+    },
+  ];
+
+  // state for cart
+  const [cartNo, setCartNo] = useState(0);
+
+  return (
+    <>
+      <NavBar cartNo={cartNo} />
+      <Header />
+      <Products products={products} setCartNo={setCartNo} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
